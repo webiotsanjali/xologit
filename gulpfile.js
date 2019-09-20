7b38cfe29458e26a85ef6e73aa442652d3e3b07f
savan19 <savan.manvar@webiots.in>
2019-09-20 13:42:16 +0530

    dashboard 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,76 @@
-'use strict';
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    pug = require('gulp-pug'),
-    autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin'),
-    htmlValidator = require('gulp-w3c-html-validator'),
-    through2 =      require('through2'),
-    bs = require('browser-sync').create();
-sass.compiler = require('node-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const pug = require('gulp-pug');
+const imagemin = require('gulp-imagemin');
+const htmlValidator = require('gulp-w3c-html-validator');
+const   through2 =      require('through2');
+const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create();
 
-// Scss to css
-gulp.task('sass', function () {
-    return gulp.src('assets/scss/**/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 versions'))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(bs.reload({stream: true}));
-});
+//scss to css
+function style() {
+  return gulp.src('assets/scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('assets/css'));
+}
 
 // pug to html
-gulp.task('pug', function () {
-      return gulp.src('assets/pug/pages/theme/index.pug')
-        .pipe(pug({ pretty: true }))
-        .on('error', console.error.bind(console))
-        .pipe(gulp.dest('theme'))
-        .pipe(bs.reload({stream: true}));
-});
+function html() {
+  return gulp.src('assets/pug/pages/theme/**.pug')
+  .pipe(pug({ pretty: true }))
+  .on('error', console.error.bind(console))
+  .pipe(gulp.dest('theme'))
+  .pipe(browserSync.reload({stream: true}));
+}
 
+//Html validator
+function validateHtml() {
+    function handleFile(file, encoding, callback) {
+        callback(null, file);
+        if (!file.w3cjs.success)
+            throw new Error('HTML validation error(s) found');
+    };
+    return gulp.src('pages/*.html')
+        .pipe(htmlValidator())
+        .pipe(through2.obj(handleFile));
+}
 
-gulp.task('watch', function () {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/pug/pages/**/*.pug', ['pug']);
-    gulp.watch('assets/virtual_images/*/*.*', ['image']);
-    //gulp.watch('ltr/*.html', ['validateHtml']);
-    gulp.watch("*.html").on('change', bs.reload);
-});
+// minify images
+function image() {
+    return gulp.src('assets/virtual_images/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('assets/images'));
+}
 
-gulp.task('browser-sync',['watch'], function() {
-    bs.init({
-        proxy: "localhost/xologit/theme/index.html"});
-});
+// minify html
+function htmlminify() {
+  return gulp.src('html/index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('html/minify'));
+}
 
-//gulp.task('default', [ 'sass', 'pug', 'image', 'watch', 'browser-sync', 'validateHtml' ]);
-gulp.task('default', [ 'sass', 'pug', 'watch']);
+// Watch function
+function watch(){
+  browserSync.init({
+      proxy: 'home/savan/Desktop/xologit/theme/vertical.html'
+  });
+  gulp.watch('assets/scss/**/*.scss', style);
+  gulp.watch('assets/pug/pages/theme/**.pug', html);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('assets/css/*.css').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.html = html;
+exports.watch = watch;
+exports.image = image;
+exports.validateHtml = validateHtml;
+exports.htmlminify = htmlminify;                
+
+const build = gulp.series(watch);
+gulp.task('default', build, 'browser-sync');
